782dca87f25fa6ee66aa95930c43f43e38ce183d
mflorczak <michal.florczak0@gmail.com>
2018-03-13 20:07:59 +0100

    Update CollectionTestSuite

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,12 +1,24 @@
 package com.kodilla.testing.collection;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class CollectionTestSuite {
+    @Before
+    public void before() {
+        System.out.println("Testing: begine");
+    }
+
+    @After
+    public void after() {
+        System.out.println("Testing: end");
+    }
+
     @Test
     public void testOddNumbersExterminatorEmptyList() {
         //Given
@@ -14,6 +26,7 @@ public class CollectionTestSuite {
         ArrayList <Integer> empty = new ArrayList<>();
         //When
         ArrayList <Integer> list = oddNumbersExterminator.exterminate(empty);
+        System.out.println("Testing: empty...");
         //Then
         Assert.assertEquals(empty.size(),list.size());
     }
@@ -24,12 +37,13 @@ public class CollectionTestSuite {
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         ArrayList <Integer> randomNumber = new ArrayList<>();
         //When
-        for(int i=0; i<50; i++) {
-            Random random = new Random();
-            randomNumber.add(random.nextInt(100));
+        for(int i=0; i<5; i++) {
+            randomNumber.add(i);
         }
+        System.out.println("Testing: normal...");
         ArrayList <Integer> evenList = oddNumbersExterminator.exterminate(randomNumber);
         //Then
+        Assert.assertEquals(evenList.size(),3);
 
     }
 }
