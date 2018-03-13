2d7ea352a5788eae1927ef549bd64915c5582758
mflorczak <michal.florczak0@gmail.com>
2018-03-13 20:12:39 +0100

    Update

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -11,7 +11,7 @@ import java.util.Random;
 public class CollectionTestSuite {
     @Before
     public void before() {
-        System.out.println("Testing: begine");
+        System.out.println("Testing: begin");
     }
 
     @After
