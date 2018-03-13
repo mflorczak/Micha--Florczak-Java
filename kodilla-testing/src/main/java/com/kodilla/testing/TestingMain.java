8d71bd49523abce68056d556cb53e36e0ab28bf3
mflorczak <michal.florczak0@gmail.com>
2018-03-13 17:12:15 +0100

    Add new tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,30 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main (String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("TheForumUser");
+
+        String result = simpleUser.getSipmleUser();
+
+        if(result.equals("TheForumUser")) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(10,15);
+        int subtractResult = calculator.subtract(13,4);
+
+        if(addResult == 25 && subtractResult == 9) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
