68fbf0f878f6766b090a124a99ba3c8b3b18dc44
mflorczak <michal.florczak0@gmail.com>
2018-03-15 19:17:36 +0100

    remove testShowFigures

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,10 +1,17 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.shape.ShapeCollector;
+import com.kodilla.testing.shape.Square;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main (String[] args) {
+        ShapeCollector shapeCollector = new ShapeCollector();
+        Square square = new Square();
+        shapeCollector.addFigure(square);
 
+       String name = shapeCollector.showFigures();
+        System.out.println(name);
     }
 }
