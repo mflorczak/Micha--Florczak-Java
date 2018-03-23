476388b92aa8524e7ff10a3b9c0b92e745413af9
mflorczak <michal.florczak0@gmail.com>
2018-03-23 19:35:04 +0100

    Module 7.4

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -0,0 +1,25 @@
+package com.kodilla.stream.world;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Continent {
+    private final String continentName;
+    private final List<Country> countries = new ArrayList<>();
+
+    public void addCountry(Country country){
+        countries.add(country);
+    }
+
+    public Continent(final String continentName) {
+        this.continentName = continentName;
+    }
+
+    public String getContinentName() {
+        return continentName;
+    }
+
+    public List<Country> getCountries() {
+        return countries;
+    }
+}
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.stream.world;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class WorldTestSuite {
+    @Test
+    public void testGetPeopleQuantity() {
+        //Given
+        World world = new World();
+        Continent europe = new Continent("Europe");
+        Continent africa = new Continent("Africa");
+        Continent asia = new Continent("Asia");
+        Country poland = new Country("Poland",new BigDecimal("333"));
+        Country irland = new Country("Irland", new BigDecimal("1000"));
+        Country slovakia = new Country("Slovakia", new BigDecimal("2000"));
+        Country china = new Country("China", new BigDecimal("3000"));
+        Country egypt = new Country("Egypt", new BigDecimal("2500"));
+        europe.addCountry(poland);
+        europe.addCountry(irland);
+        europe.addCountry(slovakia);
+        africa.addCountry(egypt);
+        asia.addCountry(china);
+        world.addContinents(europe);
+        world.addContinents(africa);
+        world.addContinents(asia);
+
+        //When
+        BigDecimal actual = world.getPeopleQuantity();
+
+        //
+        BigDecimal expected = new BigDecimal("8833");
+        Assert.assertEquals(expected,actual);
+    }
+}
