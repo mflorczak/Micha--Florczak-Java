a127db305331e69393c4de05dfd7782e256cb07e
mflorczak <michal.florczak0@gmail.com>
2018-04-09 14:10:50 +0200

    Module 9.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challengers/ValidatorOrder.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challengers/ValidatorOrder.java
@@ -2,14 +2,14 @@ package com.kodilla.good.patterns.challengers;
 
 public class ValidatorOrder {
     private  Item items;
-    public static boolean validatorOrder(SellDto sellDto) {
+    public static boolean validatorOrder(final SellDto sellDto) {
         if(sellDto.isSold()) {
             return true;
         } return false;
     }
 
 
-    public static void isSend(String name, String lastName, SellDto sellDto) {
+    public static void isSend(final String name, final String lastName, final SellDto sellDto) {
         if(name.equals(User.getName()) && lastName.equals(User.getLastName()) && validatorOrder(sellDto)) {
             System.out.println("The order for " + User.getName() + " is send.");
         } else {
