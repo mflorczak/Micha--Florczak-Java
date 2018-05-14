e8554ea3c3108eef432cc29b482f6d24aae480ef
mflorczak <michal.florczak0@gmail.com>
2018-05-14 19:18:42 +0200

    Update Sudoku

--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuGame.java
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuGame.java
@@ -11,6 +11,10 @@ public class SudokuGame {
         return board;
     }
 
+    public int getBoxNumber(int x) {
+        return x / 3;
+    }
+
     public void removePossibles(int x, int y) {
         SudokuElement el = getBoard().getColumn().getCols().get(y).getRows().get(x);
         for(int i = 0; i < 9; i++) {
@@ -22,6 +26,14 @@ public class SudokuGame {
             //Column
             if(el.getPossibleValues().contains(getBoard().getColumn().getCols().get(i).getRows().get(x).getValue())) {
                 Integer integer = getBoard().getColumn().getCols().get(i).getRows().get(x).getValue();
+                el.getPossibleValues().remove(integer);
+            }
+        }
+
+        for(int q = 0; q < 3; q++) {
+            for(int m = 0; m < 3; m++) {
+                Integer integer = getBoard().getColumn().getCols()
+                        .get(q + getBoxNumber(y)*3).getRows().get(m + getBoxNumber(x)*3).getValue();
                 el.getPossibleValues().remove(integer);
             }
         }
