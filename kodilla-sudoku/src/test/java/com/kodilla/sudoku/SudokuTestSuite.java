0f3cedd434d92db9bd7de51fd62a9a3c8004a86e
mflorczak <michal.florczak0@gmail.com>
2018-05-17 17:37:14 +0200

    update sudoku

--- kodilla-sudoku/src/test/java/com/kodilla/sudoku/SudokuTestSuite.java
+++ kodilla-sudoku/src/test/java/com/kodilla/sudoku/SudokuTestSuite.java
@@ -16,7 +16,7 @@ public class SudokuTestSuite {
     }
 
     @Test
-    public void testResolveSudoku() {
+    public void testResolveSudokuHard() {
         //Given
         SudokuGame game = new SudokuGame();
         //SUDOKU HARD
@@ -55,52 +55,63 @@ public class SudokuTestSuite {
         game.getBoard().getColumn().setElement(3,8,8);
         game.getBoard().getColumn().setElement(7,8,4);
 
-       //SUDOKU EASY
-//        game.getBoard().getColumn().setElement(1,0,4);
-//        game.getBoard().getColumn().setElement(2,0,5);
-//
-//        game.getBoard().getColumn().setElement(0,1,8);
-//        game.getBoard().getColumn().setElement(3,1,4);
-//        game.getBoard().getColumn().setElement(6,1,5);
-//        game.getBoard().getColumn().setElement(8,1,7);
-//
-//        game.getBoard().getColumn().setElement(0,2,3);
-//        game.getBoard().getColumn().setElement(3,2,2);
-//        game.getBoard().getColumn().setElement(4,2,9);
-//        game.getBoard().getColumn().setElement(6,2,4);
-//        game.getBoard().getColumn().setElement(7,2,1);
-//
-//        game.getBoard().getColumn().setElement(1,3,2);
-//        game.getBoard().getColumn().setElement(2,3,8);
-//        game.getBoard().getColumn().setElement(4,3,5);
-//        game.getBoard().getColumn().setElement(5,3,4);
-//        game.getBoard().getColumn().setElement(7,3,7);
-//        game.getBoard().getColumn().setElement(8,3,3);
-//
-//        game.getBoard().getColumn().setElement(2,4,7);
-//        game.getBoard().getColumn().setElement(3,4,9);
-//        game.getBoard().getColumn().setElement(6,4,2);
-//        game.getBoard().getColumn().setElement(7,4,6);
-//        game.getBoard().getColumn().setElement(8,4,1);
-//
-//        game.getBoard().getColumn().setElement(3,5,1);
-//
-//        game.getBoard().getColumn().setElement(1,6,3);
-//        game.getBoard().getColumn().setElement(2,6,4);
-//        game.getBoard().getColumn().setElement(4,6,6);
-//        game.getBoard().getColumn().setElement(7,6,8);
-//
-//        game.getBoard().getColumn().setElement(2,7,9);
-//        game.getBoard().getColumn().setElement(3,7,3);
-//        game.getBoard().getColumn().setElement(4,7,1);
-//        game.getBoard().getColumn().setElement(6,7,7);
-//        game.getBoard().getColumn().setElement(8,7,5);
-//
-//        game.getBoard().getColumn().setElement(1,8,8);
-//        game.getBoard().getColumn().setElement(3,8,5);
-//        game.getBoard().getColumn().setElement(4,8,4);
-//        game.getBoard().getColumn().setElement(7,8,2);
-//        game.getBoard().getColumn().setElement(8,8,6);
+        ///When & Then
+        if(game.resolveSudoku()) {
+            game.getBoard().showBoard();
+        }
+    }
+
+    @Test
+    public void testResolveSudokuEasy() {
+        //Given
+        SudokuGame game = new SudokuGame();
+
+        //SUDOKU EASY
+        game.getBoard().getColumn().setElement(1,0,4);
+        game.getBoard().getColumn().setElement(2,0,5);
+
+        game.getBoard().getColumn().setElement(0,1,8);
+        game.getBoard().getColumn().setElement(3,1,4);
+        game.getBoard().getColumn().setElement(6,1,5);
+        game.getBoard().getColumn().setElement(8,1,7);
+
+        game.getBoard().getColumn().setElement(0,2,3);
+        game.getBoard().getColumn().setElement(3,2,2);
+        game.getBoard().getColumn().setElement(4,2,9);
+        game.getBoard().getColumn().setElement(6,2,4);
+        game.getBoard().getColumn().setElement(7,2,1);
+
+        game.getBoard().getColumn().setElement(1,3,2);
+        game.getBoard().getColumn().setElement(2,3,8);
+        game.getBoard().getColumn().setElement(4,3,5);
+        game.getBoard().getColumn().setElement(5,3,4);
+        game.getBoard().getColumn().setElement(7,3,7);
+        game.getBoard().getColumn().setElement(8,3,3);
+
+        game.getBoard().getColumn().setElement(2,4,7);
+        game.getBoard().getColumn().setElement(3,4,9);
+        game.getBoard().getColumn().setElement(6,4,2);
+        game.getBoard().getColumn().setElement(7,4,6);
+        game.getBoard().getColumn().setElement(8,4,1);
+
+        game.getBoard().getColumn().setElement(3,5,1);
+
+        game.getBoard().getColumn().setElement(1,6,3);
+        game.getBoard().getColumn().setElement(2,6,4);
+        game.getBoard().getColumn().setElement(4,6,6);
+        game.getBoard().getColumn().setElement(7,6,8);
+
+        game.getBoard().getColumn().setElement(2,7,9);
+        game.getBoard().getColumn().setElement(3,7,3);
+        game.getBoard().getColumn().setElement(4,7,1);
+        game.getBoard().getColumn().setElement(6,7,7);
+        game.getBoard().getColumn().setElement(8,7,5);
+
+        game.getBoard().getColumn().setElement(1,8,8);
+        game.getBoard().getColumn().setElement(3,8,5);
+        game.getBoard().getColumn().setElement(4,8,4);
+        game.getBoard().getColumn().setElement(7,8,2);
+        game.getBoard().getColumn().setElement(8,8,6);
 
         ///When & Then
         if(game.resolveSudoku()) {
